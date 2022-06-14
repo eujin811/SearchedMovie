d0752e25195155c6c715038baaa16208bd24307e
eujin811 <eujin8@gmail.com>
2022-06-14 15:20:03 +0900

    [+] make a project

---
+++ SearchedMovie/SearchedMovie/View/Util/BasicViewController.swift
@@ -0,0 +1,44 @@
+//
+//  BasicViewController.swift
+//  SearchedMovie
+//
+//
+
+import UIKit
+
+class BasicViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setUI()
+        setConstraints()
+    }
+    
+    func setUI() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+
+}
---
+++ SearchedMovie/SearchedMovie/ViewModel/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  SearchedMovie
+//
+
+import UIKit
+
+class MainViewController: BasicViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setUI() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+}
