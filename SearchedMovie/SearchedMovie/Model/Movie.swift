ecfa007b128adaf6dc95e1211859e40c71741aa3
eujin811 <eujin8@gmail.com>
2022-06-15 22:59:47 +0900

    [+] Model

---
+++ SearchedMovie/SearchedMovie/Model/Movie.swift
@@ -0,0 +1,21 @@
+//
+//  Movie.swift
+//  SearchedMovie
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let title: String?
+    let link: String?
+    let imageURL: String?
+    let pubDate: String?
+    let director: String?
+    let actors: String?
+    let userRating: String?
+    
+//    private enum CodingKeys: String, CodingKey {
+//        
+//    }
+}
+
