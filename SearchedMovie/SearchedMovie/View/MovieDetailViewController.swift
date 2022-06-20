b42fe51b971b3716ff7ca2f3da4118c4ea853fd0
eujin811 <eujin8@gmail.com>
2022-06-21 05:30:58 +0900

    [*] MovieDetailVC: headerView

--- SearchedMovie/SearchedMovie/View/MovieDetailViewController.swift
+++ SearchedMovie/SearchedMovie/View/MovieDetailViewController.swift
@@ -4,6 +4,7 @@
 //
 
 import UIKit
+import WebKit
 
 import RxCocoa
 import RxSwift
@@ -11,6 +12,17 @@ import SnapKit
 
 class MovieDetailViewController: BasicViewController {
     private let viewModel = MovieDetailViewModel()
+    
+    private let contentsStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .equalSpacing
+        $0.alignment = .fill
+        
+        $0.backgroundColor = .lightGray
+    }
+    
+    private let headerView = DetailViewHeader()
+    let webView = WKWebView()
     
     private let isFavoriteRelay = PublishRelay<Bool>()
     
@@ -21,10 +33,30 @@ class MovieDetailViewController: BasicViewController {
     
     override func setUI() {
         super.setUI()
+//        navigationItem.backButtonTitle = ""
+//        navigationItem.backBarButtonItem?.tintColor = .gray
+        
+        
+        view.addSubview(contentsStackView)
+        
+        contentsStackView.addArrangedSubview(headerView)
+        contentsStackView.addArrangedSubview(webView)
     }
     
     override func setConstraints() {
         super.setConstraints()
+        
+        contentsStackView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        headerView.snp.makeConstraints {
+            $0.height.equalTo(100)
+        }
+//        webView.snp.makeConstraints {
+//            $0.top.leading.trailing.bottom.equalToSuperview()//.equalTo(view.safeAreaLayoutGuide)
+//        }
     }
     
     override func bind() {
@@ -32,10 +64,30 @@ class MovieDetailViewController: BasicViewController {
         
         let vmOutput = viewModel
             .transform(input: .init(isFavoriteRelay: isFavoriteRelay))
+        
+        Observable.combineLatest(
+            vmOutput.movieRelay,
+            vmOutput.isFavoriteRelay)
+        .bind(with: self) { owner, data in
+            let (movie, isFavorite) = data
+            owner.configure(movie: movie, isFavorite: isFavorite)
+        }
+        .disposed(by: disposeBag)
     }
     
     override func subscribe() {
         super.subscribe()
+        
+        headerView.didTapStarButton {
+            print("tap star")
+        }
+    }
+    
+    private func configure(movie: Movie, isFavorite: Bool) {
+        title = movie.title?.removeTag(.bTag)
+        
+        headerView.configure(movie: movie, isFavorite: isFavorite)
+        print("configure", movie.title)
     }
     
 }
