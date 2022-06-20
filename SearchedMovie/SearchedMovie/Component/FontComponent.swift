fbed92df9da8caead4dd92911fed7713cfcd1bd4
eujin811 <eujin8@gmail.com>
2022-06-21 00:37:58 +0900

    [+] Font

--- SearchedMovie/SearchedMovie/Component/FontComponent.swift
+++ SearchedMovie/SearchedMovie/Component/FontComponent.swift
@@ -10,12 +10,18 @@ import UIKit
 enum FontComponent {
     case cellTitle
     case cellSubtitle
+    case barTitle
+    case miniButton
     
     var font: UIFont {
         switch self {
         case .cellTitle:
             return UIFont.boldSystemFont(ofSize: 16)
         case .cellSubtitle:
+            return UIFont.systemFont(ofSize: 12)
+        case .barTitle:
+            return UIFont.boldSystemFont(ofSize: 24)
+        case .miniButton:
             return UIFont.systemFont(ofSize: 12)
         }
     }
