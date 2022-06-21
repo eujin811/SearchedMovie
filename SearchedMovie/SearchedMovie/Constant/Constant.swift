9b4fc06a9960c6c29b9699fa0274928af7ec5bf1
eujin811 <eujin8@gmail.com>
2022-06-21 12:05:32 +0900

    [+] DatabaseConstant

--- SearchedMovie/SearchedMovie/Constant/Constant.swift
+++ SearchedMovie/SearchedMovie/Constant/Constant.swift
@@ -9,4 +9,6 @@ enum Constant {
     static let parsingConstant = ParsingConstant.self
     static let viewID = ViewIdentifier.self
     static let view = ViewConstant.self
+    
+    static let db = DatabaseConstant.self
 }
---
+++ SearchedMovie/SearchedMovie/Constant/DatabaseConstant.swift
@@ -0,0 +1,14 @@
+//
+//  DatabaseConstant.swift
+//  SearchedMovie
+//
+//
+
+import Foundation
+
+enum DatabaseConstant {
+    static let primaryID = "id"
+    static let title = "title"
+    
+    static let equalSymbol = " == "
+}
